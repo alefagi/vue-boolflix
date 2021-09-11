9f0422b74393a9042079bb1fce9f5432bb4350d4
alefagi <iltu@libero.it>
2021-09-11 18:01:32 +0200

    created project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="container flex-between">
+        <h1>BOOLFLIX</h1>
+        <Search />
+      </div>
+    </header>
+    <main> 
+      <div class="container">
+        <section id="movies">
+          <h2>Movies</h2>
+          <Card />
+        </section>
+        <section id="series">
+          <h2>TV Series</h2>
+          <Card />
+        </section>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue';
+import Card from './components/Card.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Search,
+    Card,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/scss/style.scss";
+
+header {
+  background-color: #000;
+  height: 100px;
+  h1 {
+    color: red;
+  }
+};
+
+main {
+  background-color: grey;
 }
+
+
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text">
+    <button>Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style scoped lang="scss">
+  button {
+    padding: 5px;
+    margin-left: 10px;
+  }
+
+  input {
+    padding: 5px;
+  }
+
+</style>
