8691f8b6575d214f753256e4e1fb592b2ec991ec
alefagi <iltu@libero.it>
2021-09-12 12:10:45 +0200

    added language flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,10 @@
     <ul>
       <li>Titolo: {{ titolo }}</li>
       <li>Titolo Originale: {{ titoloOriginale }} </li>
-      <li>Lingua: {{ lingua }}</li>
+      <li>Lingua: 
+        <img v-if="!imgNotFound" :src="getLanguageFlag(lingua)" :alt="lingua">
+        <span v-else>{{ lingua }}</span>
+      </li>
       <li>Voto: {{ voto }}</li>
     </ul>
   </div>
@@ -17,10 +20,31 @@ export default {
     titoloOriginale: String,
     lingua: String,
     voto: Number,
-  }
+  },
+  data() {
+    return {
+      imgNotFound: false,
+    }
+  },
+  methods: {
+    getLanguageFlag(lingua) {
+      switch (lingua) {
+        case 'it':
+          return lingua = require ('@/assets/images/it.png');
+        case 'en':
+          return lingua = require ('@/assets/images/en.png');
+        default:
+          this.imgNotFound = true;
+          return lingua;
+      }
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
+img {
+  width: 30px;
+}
 
 </style>
